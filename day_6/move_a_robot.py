23e11e44ec241b815967ab23f043c7d29d536c7e
Jason Fox <jason.t.fox@gmail.com>
2023-12-04 04:57:06 -0500

    move a robot!

---
+++ day_6/move_a_robot.py
@@ -0,0 +1,22 @@
+# use the robot on this website:  https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+#use this code (it will only run on the website)
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
