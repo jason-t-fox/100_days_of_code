a558b49a1af11df94cfe4b22c7a09ba2bf0963e7
Jason Fox <jason.t.fox@gmail.com>
2023-12-09 06:20:40 -0500

    bug fix

--- day_16/main.py
+++ day_16/main.py
@@ -11,13 +11,13 @@ machine_running = True
 while machine_running:
     options = coffee_maker_menu.get_items()
     user_selection = input(f"What would you like? ({options}): ").lower()
+    drink = coffee_maker_menu.find_drink(user_selection)
     if user_selection == "off":
         print("Turning machine off!")
         machine_running = False
     elif user_selection == "report":
         coffee_maker.report()
         money.report()
-    else:
-        drink = coffee_maker_menu.find_drink(user_selection)
+    elif drink is not None:
         if coffee_maker.is_resource_sufficient(drink) and money.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
