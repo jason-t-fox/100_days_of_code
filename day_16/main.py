7b3a2709cf22c62e32e03fe48f5bd34c9c5cdf30
Jason Fox <jason.t.fox@gmail.com>
2023-12-09 05:54:20 -0500

    initial coffee maker oop edition commit

--- day_16/main.py
+++ day_16/main.py
@@ -1,3 +1,23 @@
-from menu import Menu, MenuItem
+from menu import Menu  # , MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker_menu = Menu()
+coffee_maker = CoffeeMaker()
+money = MoneyMachine()
+
+machine_running = True
+
+while machine_running:
+    options = coffee_maker_menu.get_items()
+    user_selection = input(f"What would you like? ({options}): ").lower()
+    if user_selection == "off":
+        print("Turning machine off!")
+        machine_running = False
+    elif user_selection == "report":
+        coffee_maker.report()
+        money.report()
+    else:
+        drink = coffee_maker_menu.find_drink(user_selection)
+        if coffee_maker.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
