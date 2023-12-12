63bfc82b8bd620ea18766bd0e7963bd6984e3a5a
Jason Fox <jfox@oakwoodsys.com>
2023-12-12 14:49:13 -0500

    Letter Writer

--- day_24/main.py
+++ day_24/main.py
@@ -6,6 +6,6 @@ start_letter_path = 'input/Letters/starting_letter.txt'
 # This doesn't need to be a class, but I want to cement earlier lessons.
 letter_writer = LetterWriter(path_to_names=names_path, path_to_letter=start_letter_path)
 
-names_list = letter_writer.get_names()
-letter_template = letter_writer.get_letter()
+letter_writer.get_names()
+letter_writer.get_letter()
 letter_writer.write_files()
