bf2d7d185c53272484382c452552a7acce5ae78d
Jason Fox <jfox@oakwoodsys.com>
2023-12-12 14:36:16 -0500

    Letter Writer

--- day_24/LetterWriter.py
+++ day_24/LetterWriter.py
@@ -2,20 +2,23 @@ import os
 
 
 class LetterWriter:
-    @staticmethod
-    def get_names(path_to_names):
-        with open(path_to_names, "r") as names_file:
+    def __init__(self, path_to_names, path_to_letter):
+        self.path_to_names = path_to_names
+        self.path_to_letter = path_to_letter
+
+    def get_names(self):
+        with open(self.path_to_names, "r") as names_file:
             names_list = [line.strip() for line in names_file.readlines()]
         return names_list
 
-    @staticmethod
-    def get_letter(path_to_letter):
-        with open(path_to_letter, "r") as input_letter_file:
+    def get_letter(self):
+        with open(self.path_to_letter, "r") as input_letter_file:
             letter_template = input_letter_file.readlines()
         return letter_template
 
-    @staticmethod
-    def write_files(names_list, letter_template):
+    def write_files(self):
+        names_list = self.get_names()
+        letter_template = self.get_letter()
         for name in names_list:
             letter_contents = [s.replace('[name]', name) for s in letter_template]
             letter_output = "".join(letter_contents)
