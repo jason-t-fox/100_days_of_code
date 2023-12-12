25d8a6b213af9d588324ccf6b3978904827179f5
Jason Fox <jfox@oakwoodsys.com>
2023-12-12 14:46:26 -0500

    Letter Writer

--- day_24/main.py
+++ day_24/main.py
@@ -1,8 +1,7 @@
-import os
 from LetterWriter import LetterWriter
 
-names_path = os.path.join(os.path.dirname(__file__), 'input', 'Names', 'invited_names.txt')
-start_letter_path = os.path.join(os.path.dirname(__file__), 'input', 'Letters', 'starting_letter.txt')
+names_path = 'input/Names/invited_names.txt'
+start_letter_path = 'input/Letters/starting_letter.txt'
 
 letter_writer = LetterWriter(path_to_names=names_path, path_to_letter=start_letter_path)
 
