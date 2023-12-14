1042c4039a70dad937df91ecd06d1c345acc9136
Jason Fox <jason.t.fox@gmail.com>
2023-12-14 07:25:07 -0500

    day 27

--- day_27/tkinter_fun.py
+++ day_27/tkinter_fun.py
@@ -1,12 +1,25 @@
 import tkinter
+# from time import sleep
 
 window = tkinter.Tk()
 window.title("I am a title!")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="label1", font=("Ariel", 24, "bold"))
-other_label = tkinter.Label(text="label2", font=("Ariel", 24, "bold"))
-my_label.pack(side="left")
-other_label.pack(side="left")
+my_label["text"] = "new text"
+
+input_field = tkinter.Entry(width=10)
+
+
+def button_clicked():
+    my_label["text"] = input_field.get()
+
+
+button1 = tkinter.Button(text="click me", command=button_clicked)
+
+
+my_label.pack()
+button1.pack()
+input_field.pack()
 
 window.mainloop()
