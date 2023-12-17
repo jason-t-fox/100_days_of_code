b89c76f856460089655bf674e39c25f259aa2101
Jason Fox <jason.t.fox@gmail.com>
2023-12-17 06:11:40 -0500

    day 32

---
+++ day_32/main.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
+SMTP_ADDRESS = "smtp.fake_address.ReplaceMe"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:  # just iterating over the raw data won't scale well.  For larger data,
+    # select today's birthdays out of the original dataframe first.
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP(SMTP_ADDRESS) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
