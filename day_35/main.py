28dc7f45b361e48250faf4cc07c23a2b9eb58612
Jason Fox <jfox@oakwoodsys.com>
2023-12-18 16:45:12 -0500

    day 35

--- day_35/main.py
+++ day_35/main.py
@@ -1,15 +1,38 @@
 import requests
-from apikey import api_key  # actual API key will not be stashed in git
+from twilio.rest import Client
+import apikey  # api keys in separate file (not in git)
 
-# Rock City Chattanooga TN
-latitude = 34.975307
-longitude = -85.354826
-url = "https://api.openweathermap.org/data/2.5/forecast"
-parameters = {
-    "lat": latitude,
-    "lon": longitude,
-    "appid": api_key
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = apikey.openweather
+account_sid = apikey.twilio_sid
+auth_token = apikey.twilio_auth
+
+# Rock City Chattanooga TN, it's a cool place to visit!
+MY_LAT = 34.975307
+MY_LONG = -85.354826
+
+weather_params = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "cnt": 4,
 }
 
-weather_data = requests.get(url, params=parameters)
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
 
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today.",
+        from_="111-111-1111",
+        to="222-222-2222"
+    )
+    print(message.status)
