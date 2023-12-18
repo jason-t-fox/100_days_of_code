272dc2ca859bd0b5b03c16889844d13dfb20380e
Jason Fox <jfox@oakwoodsys.com>
2023-12-18 16:11:31 -0500

    updating .gitignore

---
+++ day_35/apikey.py
@@ -0,0 +1,1 @@
+api_key = "5a74782ab7fc3903b8aabe7eec0cb98a"
---
+++ day_35/main.py
@@ -0,0 +1,15 @@
+import requests
+from apikey import api_key  # actual API key will not be stashed in git
+
+# Rock City Chattanooga TN
+latitude = 34.975307
+longitude = -85.354826
+url = "https://api.openweathermap.org/data/2.5/forecast"
+parameters = {
+    "lat": latitude,
+    "lon": longitude,
+    "appid": api_key
+}
+
+weather_data = requests.get(url, params=parameters)
+print(weather_data.json())
