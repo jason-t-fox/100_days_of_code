3e1d6dce84e5146308c59ec0b3808efeffbe45f8
Jason Fox <jason.t.fox@gmail.com>
2023-12-23 06:31:37 -0500

    reorganizing files.

--- days 21 to 40/day_24/main.py
+++ days 21 to 40/day_24/main.py
@@ -1,7 +1,7 @@
 from LetterWriter import LetterWriter
 
-names_path = 'input/Names/invited_names.txt'
-start_letter_path = 'input/Letters/starting_letter.txt'
+names_path = 'input/names/invited_names.txt'
+start_letter_path = 'input/letters/starting_letter.txt'
 
 # This doesn't need to be a class, but I want to cement earlier lessons.
 letter_writer = LetterWriter(path_to_names=names_path, path_to_letter=start_letter_path)
